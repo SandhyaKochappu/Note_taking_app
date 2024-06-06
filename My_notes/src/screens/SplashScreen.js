0b885e386000e07f1826d6299356151bc6c50cd2
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-06 10:23:58 +1000

    Create SplashScreen.js
    Created SplashScreen

---
+++ My_notes/src/screens/SplashScreen.js
@@ -0,0 +1,36 @@
+// src/screens/SplashScreen.js
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('Login');
+    }, 6000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.png')} style={styles.logo} />
+      <Text style={styles.text}>Note Taking App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 24,
+  },
+});
+
+export default SplashScreen;
