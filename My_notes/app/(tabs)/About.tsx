0e49c2413737f189ade6f1156e1460705c7f9a95
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-07 19:23:14 +1000

    Create About.tsx

---
+++ My_notes/app/(tabs)/About.tsx
@@ -0,0 +1,25 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default function About() {
+
+
+  return (
+        <View style={styles.container}>
+      <Text>About</Text>
+      <Text>This is a note-taking application.</Text>
+    </View>
+
+
+    );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+});
