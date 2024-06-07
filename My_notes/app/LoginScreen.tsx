9235e774218e83de4fe627505b687fc7ba0b92c4
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-07 12:21:36 +1000

    Update LoginScreen.tsx

--- My_notes/app/LoginScreen.tsx
+++ My_notes/app/LoginScreen.tsx
@@ -1,26 +1,50 @@
 // src/screens/LoginScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert, ActivityIndicator } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Link, Stack } from 'expo-router';
+import { Link, Stack, useRouter } from 'expo-router';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { useRouter } from 'expo-router';
-import { login } from '../utils/api';
-
 
 export default function LoginScreen ()  { 
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/auth/login', {
+        username,
+        password,
+      });
+      await AsyncStorage.setItem('token', response.data.token);
+      router.push('/notes');
+    } catch (err) {
+      console.error(err);
+      Alert.alert('Login failed', 'Invalid username or password');
+    }
+  };
   return (
     <>
-      <Stack.Screen options={{ title: 'Login/Sign-Up' }} />
+      <Stack.Screen options={{ title: 'Login' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
+        <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
       </ThemedView>
       </>
   );
@@ -32,11 +56,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+  },  
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 75,
+  }
 });
 
 
