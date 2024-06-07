d0615cff65a4eb58a8fbe19b57ba8fe93bc927fa
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-07 22:39:42 +1000

    Create generateLicenses.js

---
+++ My_notes/generateLicenses.js
@@ -0,0 +1,19 @@
+// generateLicenses.js
+var crawler = require('npm-license-crawler'),
+    options = {
+        start: ['../..'],
+        exclude: ['.'],
+        json: 'licenses.json',
+        unknown: true
+    };
+
+crawler.dumpLicenses(options,
+    function(error, res){
+        if (error) {
+            console.error("Error:", error);
+        }
+        else {
+            console.dir(res);
+        }
+    }
+);
