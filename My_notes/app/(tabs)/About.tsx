0d848798683453e087bc3583ab9182b49cb2ac4a
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-07 22:39:08 +1000

    Update About.tsx

--- My_notes/app/(tabs)/About.tsx
+++ My_notes/app/(tabs)/About.tsx
@@ -1,25 +1,53 @@
-import { Stack } from 'expo-router';
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 
+import { View, Text, ScrollView, StyleSheet, FlatList } from 'react-native';
+
+
+export interface ILicense {
+  licenses: string;
+  repository: string;
+  licenseUrl: string;
+  parents: string;
+}
+
+interface IFinalLicense {
+  name: string;
+  version: string;
+  licenseSpecs: ILicense;
+}
 
 export default function About() {
+  render() {
+  
+    // LET'S GET THE LICENSES AND DEFINE OUR REGEXES
+    const licenses: { [id: string]: ILicense } = require('./licenses.json');
+    const numberRegex = /\d+(\.\d+)*/;
+    const atRegex = /(?:@)/gi;
 
+    const finalLicense: IFinalLicense[] = [];
+    map(licenses, (librarySpecs, libraryName) => {
+      // Extract the version of the library from the name
+      const version = libraryName.match(numberRegex);
+      // Removes the part after the @ 
+      const nameWithoutVersion = libraryName.replace(atRegex, '').replace(version ? version[0] : '', '');
+      
+      finalLicense.push({ name: nameWithoutVersion, version: version ? version[0] : '', licenseSpecs: librarySpecs });
+    });
 
-  return (
-        <View style={styles.container}>
-      <Text>About</Text>
-      <Text>This is a note-taking application.</Text>
-    </View>
-
-
+    return (
+      <View style={styles.root}>
+        <NavBarBackground isTopTabbedView={false} />
+        <FlatList
+          keyboardShouldPersistTaps={'handled'}
+          data={finalLicense}
+          contentContainerStyle={styles.listContainer}
+          renderItem={({ item }) => {
+            return this.renderItem(item);
+          }}
+          ItemSeparatorComponent={this.renderSeparator}
+          extraData={this.state}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
     );
+  }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-  },
-});
