c9da6dd99684f9dd26172cbc0fd9b7ada3ccac3f
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-08 22:51:52 +1000

    Update index.js

--- notes_server/routes/index.js
+++ notes_server/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.json({ unit: "IFN666" });
 });
 
+router.get("/api/city", async (req, res) => {
+  try {
+    const cities = await req.db.from("city").select("name", "district");
+    res.json({ error: false, cities });
+  } catch (error) {
+    res.json({ error: true, message: error });
+  }
+});
 module.exports = router;
+
+router.get("/api/city/:CountryCode", async (req, res) => {
+  try {
+    const cities = await req.db
+                  .from("city")
+                  .select("name", "district")
+                  .where("CountryCode", req.params.CountryCode);
+    res.json({ error: false, cities });
+  } catch (error) {
+    res.json({ error: true, message: error });
+  }
+});
