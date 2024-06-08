44c17a8c1b0323a85188d29a7d4554b33b30f6b1
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-08 20:47:45 +1000

    Update _layout.tsx

--- My_notes/app/_layout.tsx
+++ My_notes/app/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import { FontThemeProvider } from "../context/theme";
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -28,6 +28,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <FontThemeProvider>
       <Stack
         screenOptions={{
         headerStyle: {
@@ -42,6 +43,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+      </FontThemeProvider>
     </ThemeProvider>
   );
 }
