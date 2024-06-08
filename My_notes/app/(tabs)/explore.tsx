c60771138dfa9db73d219b1b556156965aa3f1da
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-08 20:46:48 +1000

    Update explore.tsx

--- My_notes/app/(tabs)/explore.tsx
+++ My_notes/app/(tabs)/explore.tsx
@@ -1,40 +1,16 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform, View, Text } from 'react-native';
-import Slider  from '../../node_modules/@react-native-community/slider';
+import { StyleSheet, Image, Platform, View, Text, Switch } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const FONT_SIZE_KEY = '@font_size';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useTheme } from "../../context/theme";
+import { GlobalStyles } from "../../styles/global";
 
 export default function TabTwoScreen() {
-  const [fontSize, setFontSize] = useState<number>(16);
-
-  useEffect(() => {
-    const loadSettings = async () => {
-      try {
-        const savedFontSize = await AsyncStorage.getItem(FONT_SIZE_KEY);
-        if (savedFontSize !== null) {
-          setFontSize(parseFloat(savedFontSize));
-        }
-      } catch (error) {
-        console.error('Failed to load settings.', error);
-      }
-    };
-
-    loadSettings();
-  }, []);
-
-  const handleFontSizeChange = async (value: number) => {
-    setFontSize(value);
-    try {
-      await AsyncStorage.setItem(FONT_SIZE_KEY, value.toString());
-    } catch (error) {
-      console.error('Failed to save settings.', error);
-    }
-  };
+  const [isLargeText, setIsLargeText] = useState(false);  
+  const globalStyles = GlobalStyles(); 
 
   return (
     <ParallaxScrollView
@@ -44,19 +20,14 @@ export default function TabTwoScreen() {
         <ThemedText type="title">Settings</ThemedText>
       </ThemedView>
       <ThemedText>You can change the accessibilty settings here.</ThemedText>
-      
-      <View style={styles.container}>
-      <Text style={[styles.label, { fontSize }]}>Sample Text</Text>
-      <Text style={styles.label}>Font Size: {fontSize.toFixed(0)}</Text>
-      <Slider
-        style={styles.slider}
-        minimumValue={12}
-        maximumValue={30}
-        step={1}
-        value={fontSize}
-        onValueChange={handleFontSizeChange}
-      />
-    </View>
+      <View style={styles.view}>
+        <Switch
+          value={isLargeText}
+          onValueChange={() => setIsLargeText(!isLargeText)}
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+        />
+        <Text style={globalStyles.text}>Large Text</Text>
+      </View>      
 
     </ParallaxScrollView>
   );
@@ -73,17 +44,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: 8,
   },
-  container: {
-    flex: 1,
-    padding: 16,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  slider: {
-    width: 300,
+  
+  view: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
