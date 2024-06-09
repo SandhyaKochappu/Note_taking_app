777565334f9d04b6a15e40b2cfeebde460dfa761
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-09 13:59:05 +1000

    Create authContext.js

---
+++ My_notes/context/authContext.js
@@ -0,0 +1,24 @@
+// context for the user's authentication
+import React, { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+
+  const login = (token) => {
+    setToken(token);
+  };
+
+  const logout = () => {
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
