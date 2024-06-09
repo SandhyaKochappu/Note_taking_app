83939b2cea633f1d7f65feafca0b6309464a5107
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-09 13:58:54 +1000

    Update db.js

--- notes_server/db.js
+++ notes_server/db.js
@@ -1,11 +1,10 @@
-
+//instead of knex.js, use this file
   //   host: process.env.DB_HOST,
   //   user: process.env.DB_USER,
   //   password: process.env.DB_PASSWORD,
   //   database: process.env.DB_NAME,
   // })
  
-
   module.exports = {
   client: "mysql2",
   connection: {
