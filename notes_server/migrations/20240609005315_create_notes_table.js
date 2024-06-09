3ec345980a1f9797231ed17d85530909bcf6aa2c
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-09 13:58:31 +1000

    Create 20240609005315_create_notes_table.js

---
+++ notes_server/migrations/20240609005315_create_notes_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('notes', function(table) {
+    table.increments('id').primary();
+    table.integer('user_id').unsigned().notNullable().references('id').inTable('users').onDelete('CASCADE');
+    table.string('content').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('notes');
+};
