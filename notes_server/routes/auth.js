42a839e8f89733e283c562c2923f6c4f36398357
SandhyaKochappu <106261211+SandhyaKochappu@users.noreply.github.com>
2024-06-14 17:03:09 +1000

    Update auth.js

--- notes_server/routes/auth.js
+++ notes_server/routes/auth.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const db = require('../db/knex');
 const knex = require('knex')(require('../knexfile').development);
-const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 const app = express();
 
